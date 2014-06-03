806f70b8ef267bcfefb3278f6447190e22210933
Penny <ipennywong@gmail.com>
2014-06-03 09:56:27 +0800

    Upload week-3-assignment-2

--- assignments/week-3-assignment-2/src/edu/vuum/mocca/SimpleSemaphore.java
+++ assignments/week-3-assignment-2/src/edu/vuum/mocca/SimpleSemaphore.java
@@ -1,7 +1,6 @@
 package edu.vuum.mocca;
 
 import java.util.concurrent.locks.ReentrantLock;
-import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.Condition;
 
 /**
@@ -12,58 +11,101 @@ import java.util.concurrent.locks.Condition;
  *        "Fair" and "NonFair" semaphore semantics, just liked Java Semaphores.
  */
 public class SimpleSemaphore {
-    /**
-     * Define a ReentrantLock to protect the critical section.
-     */
-    // TODO - you fill in here
+	/**
+	 * Define a ReentrantLock to protect the critical section.
+	 */
+	// TODO - you fill in here
+	private ReentrantLock lock;
 
-    /**
-     * Define a ConditionObject to wait while the number of
-     * permits is 0.
-     */
-    // TODO - you fill in here
+	/**
+	 * Define a ConditionObject to wait while the number of permits is 0.
+	 */
+	// TODO - you fill in here
+	private Condition permitCondition;
 
-    /**
-     * Define a count of the number of available permits.
-     */
-    // TODO - you fill in here.  Make sure that this data member will
-    // ensure its values aren't cached by multiple Threads..
+	private int permits;
 
-    public SimpleSemaphore(int permits, boolean fair) {
-        // TODO - you fill in here to initialize the SimpleSemaphore,
-        // making sure to allow both fair and non-fair Semaphore
-        // semantics.
-    }
+	/**
+	 * Define a count of the number of available permits.
+	 */
+	// TODO - you fill in here. Make sure that this data member will
+	// ensure its values aren't cached by multiple Threads..
 
-    /**
-     * Acquire one permit from the semaphore in a manner that can be
-     * interrupted.
-     */
-    public void acquire() throws InterruptedException {
-        // TODO - you fill in here.
-    }
+	public SimpleSemaphore(int permits, boolean fair) {
+		// TODO - you fill in here to initialize the SimpleSemaphore,
+		// making sure to allow both fair and non-fair Semaphore
+		// semantics.
+		this.permits = permits;
+		lock = new ReentrantLock(fair);
+		permitCondition = lock.newCondition();
+	}
 
-    /**
-     * Acquire one permit from the semaphore in a manner that cannot be
-     * interrupted.
-     */
-    public void acquireUninterruptibly() {
-        // TODO - you fill in here.
-    }
+	/**
+	 * Acquire one permit from the semaphore in a manner that can be
+	 * interrupted.
+	 */
+	public void acquire() throws InterruptedException {
+		// TODO - you fill in here.
+		try {
+			lock.lockInterruptibly();
 
-    /**
-     * Return one permit to the semaphore.
-     */
-    void release() {
-        // TODO - you fill in here.
-    }
+			while (permits <= 0) {
+				permitCondition.await();
+			}
 
-    /**
-     * Return the number of permits available.
-     */
-    public int availablePermits() {
-        // TODO - you fill in here by changing null to the appropriate
-        // return value.
-        return null;
-    }
+			permits--;
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	/**
+	 * Acquire one permit from the semaphore in a manner that cannot be
+	 * interrupted.
+	 */
+	public void acquireUninterruptibly() {
+		// TODO - you fill in here.
+		try {
+			lock.lock();
+
+			while (permits <= 0) {
+				permitCondition.awaitUninterruptibly();
+			}
+
+			permits--;
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	/**
+	 * Return one permit to the semaphore.
+	 */
+	void release() {
+		// TODO - you fill in here.
+		try {
+			lock.lock();
+			
+			permits++;
+			
+			permitCondition.signalAll();
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	/**
+	 * Return the number of permits available.
+	 */
+	public int availablePermits() {
+		// TODO - you fill in here by changing null to the appropriate
+		// return value.
+		try {
+			lock.lock();
+			
+			return permits;
+		} finally {
+			lock.unlock();
+		}
+	}
 }
